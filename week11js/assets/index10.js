0f4cf0b8628d14092e76e7af268b167df8bd009d
FalykhovaLA <FalykhovaLA@gmail.com>
2023-01-29 22:09:46 +0300

    fixed

--- week11js/assets/index10.js
+++ week11js/assets/index10.js
@@ -1,9 +1,9 @@
-let result = document.getElementsByClassName('.result');
-let sumOne=document.getElementsByClassName('.sum1');
-let sumTwo=document.getElementsByClassName('.sum2');
-let sumThree=document.getElementsByClassName('.sum3');
-let sumFour=document.getElementsByClassName('.sum4');
-let button=document.getElementsByClassName('.discountButton');
+let result = document.querySelector('.result');
+let sumOne=document.querySelector('.sum1');
+let sumTwo=document.querySelector('.sum2');
+let sumThree=document.querySelector('.sum3');
+let sumFour=document.querySelector('.sum4');
+let button=document.querySelector('.discountButton');
 
 result.textContent = (`${+(sumOne.innerHTML)+ +(sumTwo.innerHTML) + +(sumThree.innerHTML) + +(sumFour.innerHTML)} руб.`);
 
@@ -11,4 +11,6 @@ button.addEventListener('click', makeDiscount);
 function makeDiscount() {
     let countDiscount=(`${+(sumOne.innerHTML)*0.2 + +(sumTwo.innerHTML)*0.2 + +(sumThree.innerHTML)*0.2 + +(sumFour.innerHTML)*0.2}`);
     result.textContent = (`${+(sumOne.innerHTML) + +(sumTwo.innerHTML)+ +(sumThree.innerHTML)+ +(sumFour.innerHTML) - countDiscount} руб.`); 
+    button.disabled=true;
+    button.style.cursor="default";
 }
